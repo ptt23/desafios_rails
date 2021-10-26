cc4e04ac035a0514989a3791a869c7a54e1491d0
ptt23 <lorenzo@etiner.com>
2021-10-26 20:32:00 -0300

    metodo numero cuenta

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -1,1 +1,19 @@
-cuenta_bancaria.rb
+
+
+#Se crea clase Cuenta_bancaria
+
+class CuentaBancaria
+# Se crea Getter y Setter para nombre usuario
+    attr_accessor :nombre_usuario
+# Se Crea constructor que recibe nombre_usuario y numero_cuenta
+    def initialize (nombre_usuario, numero_cuenta, )
+    #Se levanta un error en caso que número de cuenta sea distinto de 8
+        raise ArgumentError, 'numero de cuenta superior a 8' if numero_cuenta.digits.count != 8
+        @nombre_usuario = nombre_usuario
+        @numero_cuenta = numero_cuenta
+
+
+    end
+
+    
+end
